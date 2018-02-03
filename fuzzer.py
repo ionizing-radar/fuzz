570577ae967df3b14dee0b41ab7f6edba81e562f
kinetic energy released per unit mass <kerma@ionizing-radar.ca>
2018-02-03 11:04:01 +0000

    weapons need sights

--- fuzzer.py
+++ fuzzer.py
@@ -14,14 +14,14 @@ else:
 
 buffer=["A"]
 counter=100
-while len(buffer) <= 30:
+while len(buffer) <= 50:
    buffer.append("A"*counter)
    counter=counter+200
 
 for string in buffer:
    print "Fuzzing PASS with %s bytes" % len(string)
    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   connect = s.connect(('target',25))
+   connect = s.connect((target, 110))
    s.recv(1024)
    s.send('USER test\r\n')
    s.recv(1024)
